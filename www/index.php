89e9472f49d7c51c5f4a75be95f59ce4b804d3dc
daiyuchi <daiyuchi@gms.tku.edu.tw>
2019-05-16 05:56:32 +0000

    sign in page

--- www/index.php
+++ www/index.php
@@ -1,1 +1,14 @@
-index package
+<! --https://www.youtube.com/watch?v=OWNxUVnY3pg-->
+<html>
+<head>
+	<title>Login Form Design</title>
+	<link rel="stylesheet" type="text/css" href = "style.css">
+</head>
+<body>
+	<div class = "loginbox">
+		<img src="user.png" class="avatar" width="150" height="150">
+	</div>
+
+
+</body>
+</html>
