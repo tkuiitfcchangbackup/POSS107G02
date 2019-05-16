c87adbf78fb4af8bba244d93b748639e9051d95c
daiyuchi <daiyuchi@gms.tku.edu.tw>
2019-05-16 07:54:56 +0000

    modified the file sign in part

--- www/index.php
+++ www/index.php
@@ -1,4 +1,4 @@
-<! --https://www.youtube.com/watch?v=OWNxUVnY3pg-->
+<!--https://www.youtube.com/watch?v=OWNxUVnY3pg-->
 <html>
 <head>
 	<title>Login Form Design</title>
@@ -6,7 +6,19 @@
 </head>
 <body>
 	<div class = "loginbox">
-		<img src="user.png" class="avatar" width="150" height="150">
+		<img src="user.png" class="avatar">
+		<h1>Login Here</h1>
+		<form>
+		<p>Username</p>
+		<input type="text" name="" placeholder="Enter Username">
+		<p>Passward</p>
+		<input type="password" name="" placeholder = "Enter Password">
+		<input type="submit" name = "" value="Login"><br>
+		<a href= "#">Lost your password</a><br>
+		<a href= "#">Don't have an account</a><br>
+		<a href="http://163.13.22.125/POSS107G07">reference</a>
+		</form> 
+
 	</div>
 
 
